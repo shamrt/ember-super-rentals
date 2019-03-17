343a421cf04ca364aff448f47bcb252e3eb724d0
Shane Martin <shane.martin@rewardops.com>
2019-03-16 21:38:20 -0400

    feat: add rental data to rentals page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,27 +1,34 @@
 /* eslint-disable no-unused-vars */
-import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from "qunit";
+import { visit, currentURL, click } from "@ember/test-helpers";
+import { setupApplicationTest } from "ember-qunit";
 
-module('Acceptance | list rentals', function(hooks) {
+module("Acceptance | list rentals", function(hooks) {
   setupApplicationTest(hooks);
 
-  test('should show rentals as the home page', async function (assert) {
+  test("should show rentals as the home page", async function(assert) {
+    await visit("/");
+    assert.equal(currentURL(), "/rentals", "should redirect automatically");
   });
 
-  test('should link to information about the company.', async function (assert) {
+  test("should link to information about the company.", async function(assert) {
+    await visit("/");
+    await click(".menu-about");
+    assert.equal(currentURL(), "/about");
   });
 
-  test('should link to contact information.', async function (assert) {
+  test("should link to contact information.", async function(assert) {
+    await visit("/");
+    await click(".menu-contact");
+    assert.equal(currentURL(), "/contact");
   });
 
-  test('should list available rentals.', async function (assert) {
+  test("should list available rentals.", async function(assert) {
+    await visit("/");
+    assert.equal(this.element.querySelectorAll(".listing").length, 3);
   });
 
-  test('should filter the list of rentals by city.', async function (assert) {
-  });
+  test("should filter the list of rentals by city.", async function(assert) {});
 
-  test('should show details for a selected rental', async function (assert) {
-  });
-
+  test("should show details for a selected rental", async function(assert) {});
 });
